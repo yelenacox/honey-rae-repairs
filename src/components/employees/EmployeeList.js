75d32501e99d08b5dd4d3567a6bdfe472f3f4d38
Yelena Cox <yelenacox@gmail.com>
2022-10-17 21:45:37 -0500

    updated employee view

---
+++ src/components/employees/EmployeeList.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+import { Employee } from "./Employee"
+import "./Employees.css"
+
+export const EmployeeList = () => {
+    const [employees, setEmployees] = useState([])
+
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/users?isStaff=true`)
+            .then(response => response.json())   
+            .then((employeeArray) => {
+                setEmployees(employeeArray)
+            })
+        }, []
+    )
+
+    return <article className="employees">
+    {
+        employees.map(employee => <Employee key={`employee--${employee.id}`}
+            id={employee.id} 
+            fullName={employee.fullName} 
+            email={employee.email} /> )
+    }
+    </article>
+}
